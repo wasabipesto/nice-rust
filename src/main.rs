ccdcb762e3e134f5a12812167d4c3b63f2e34a6c
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-03-26 12:07:19 -0400

    batch changes

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use std::env;
 use std::collections::HashMap;
+use std::env;
 
 extern crate clap;
 use clap::Parser;
 
 extern crate malachite;
-use malachite::{Natural, num::conversion::traits::Digits};
 use malachite::num::arithmetic::traits::Pow;
+use malachite::{num::conversion::traits::Digits, Natural};
 
 use std::time::Instant;
 
 extern crate reqwest;
 extern crate serde;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 const CLIENT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -20,31 +20,33 @@ const CLIENT_VERSION: &str = env!("CARGO_PKG_VERSION");
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[arg(
-        short, 
-        long, 
-        default_value="anonymous",
-        help="the username to send alongside your contribution"
+        short,
+        long,
+        default_value = "anonymous",
+        help = "the username to send alongside your contribution"
     )]
     username: String,
 
     #[arg(
-        long, 
-        default_value="https://nicenumbers.pages.dev/api",
-        help="the base API URL to connect to"
-    )]
-    api_base: String,
-
-    #[arg(
         long,
-        help="run an offline benchmark"
+        default_value = "https://nicenumbers.net/api",
+        help = "the base API URL to connect to"
     )]
+    api_url: String,
+
+    #[arg(short, long, help = "request a range in a specific base")]
+    base: Option<u32>,
+
+    #[arg(short = 'r', long, help = "request a differently-sized range")]
+    max_range: Option<u128>,
+
+    #[arg(short, long, help = "only look for 100% nice numbers")]
+    niceonly: bool,
+
+    #[arg(long, help = "run an offline benchmark")]
     benchmark: bool,
 
-    #[arg(
-        short,
-        long,
-        help="suppress some output"
-    )]
+    #[arg(short, long, help = "suppress some output")]
     quiet: bool,
 }
 
@@ -56,9 +58,6 @@ struct FieldClaim {
     search_start: u128,
     #[serde(deserialize_with = "deserialize_stringified_number")]
     search_end: u128,
-    //claimed_time: String,
-    //claimed_by: String,
-    //expiration_time: String
 }
 
 #[derive(Debug, Serialize)]
@@ -66,8 +65,8 @@ struct FieldSubmit<'me> {
     id: u32,
     username: &'me str,
     client_version: &'static str,
-    unique_count: HashMap<u32,u32>,
-    near_misses: HashMap<u128,u32>
+    unique_count: HashMap<u32, u32>,
+    near_misses: HashMap<u128, u32>,
 }
 
 // get a static field for benchmarking
@@ -97,18 +96,29 @@ where
 }
 
 // get a field from the server - detailed
-fn get_field_detailed(api_base: &str, username: &str) -> FieldClaim {
-    let query_url = api_base.to_owned() + &"/claim/detailed?username=".to_owned() + username;
-    let claim_data: Result<FieldClaim, reqwest::Error> = reqwest::blocking::get(query_url)
-        .unwrap().json();
+fn get_field_detailed(
+    api_url: &str,
+    username: &str,
+    base: &Option<u32>,
+    max_range: &Option<u128>,
+) -> FieldClaim {
+    let mut query_url = api_url.to_owned() + &"/claim/detailed?username=".to_owned() + username;
+    if let Some(base_val) = base {
+        query_url += &("&base=".to_owned() + &base_val.to_string());
+    }
+    if let Some(max_range_val) = max_range {
+        query_url += &("&max_range=".to_owned() + &max_range_val.to_string());
+    }
+    let claim_data: Result<FieldClaim, reqwest::Error> =
+        reqwest::blocking::get(query_url).unwrap().json();
     claim_data.unwrap()
 }
 
 // submit field data to the server - detailed
-fn submit_field_detailed(api_base: &str, submit_data: FieldSubmit) {
+fn submit_field_detailed(api_url: &str, submit_data: FieldSubmit) {
     let client = reqwest::blocking::Client::new();
     let response = client
-        .post(api_base.to_owned() + &"/submit/detailed")
+        .post(api_url.to_owned() + &"/submit/detailed")
         .json(&submit_data)
         .send();
 
@@ -132,7 +142,6 @@ fn submit_field_detailed(api_base: &str, submit_data: FieldSubmit) {
 
 // get the number of unique digits in the concatenated sqube of a specified number
 fn get_num_uniques(num: Natural, base: u32) -> u32 {
-
     // create a boolean array that represents all possible digits
     let mut digits_indicator: Vec<bool> = vec![false; base as usize];
 
@@ -140,7 +149,7 @@ fn get_num_uniques(num: Natural, base: u32) -> u32 {
     let squared = (&num).pow(2);
 
     // convert to base & save the digits in the array
-    for digit in squared.to_digits_asc(&base) { 
+    for digit in squared.to_digits_asc(&base) {
         digits_indicator[digit as usize] = true;
     }
 
@@ -148,66 +157,51 @@ fn get_num_uniques(num: Natural, base: u32) -> u32 {
     let cubed = squared * num;
 
     // convert to base & save the digits in the array
-    for digit in cubed.to_digits_asc(&base) { 
+    for digit in cubed.to_digits_asc(&base) {
         digits_indicator[digit as usize] = true;
     }
 
     // output the number of unique digits
     let mut unique_digits = 0;
     for digit in digits_indicator {
-        if digit {unique_digits += 1}
+        if digit {
+            unique_digits += 1
+        }
     }
-    return unique_digits
+    return unique_digits;
 }
 
 #[test]
 fn test_get_num_uniques() {
+    assert_eq!(get_num_uniques(Natural::from(69 as u128), 10), 10);
+    assert_eq!(get_num_uniques(Natural::from(256 as u128), 2), 2);
+    assert_eq!(get_num_uniques(Natural::from(123 as u128), 8), 8);
+    assert_eq!(get_num_uniques(Natural::from(15 as u128), 16), 5);
+    assert_eq!(get_num_uniques(Natural::from(100 as u128), 99), 3);
     assert_eq!(
-        get_num_uniques(Natural::from(69 as u128), 10), 
-        10
-    );
-    assert_eq!(
-        get_num_uniques(Natural::from(256 as u128), 2), 
-        2
-    );
-    assert_eq!(
-        get_num_uniques(Natural::from(123 as u128), 8), 
-        8
-    );
-    assert_eq!(
-        get_num_uniques(Natural::from(15 as u128), 16), 
-        5
-    );
-    assert_eq!(
-        get_num_uniques(Natural::from(100 as u128), 99), 
-        3
-    );
-    assert_eq!(
-        get_num_uniques(Natural::from(4134931983708 as u128), 40), 
+        get_num_uniques(Natural::from(4134931983708 as u128), 40),
         39
     );
     assert_eq!(
-        get_num_uniques(Natural::from(173583337834150 as u128), 44), 
+        get_num_uniques(Natural::from(173583337834150 as u128), 44),
         41
     );
 }
 
 // get detailed niceness data on a range of numbers and aggregate it
-fn process_range_detailed(n_start: u128, n_end: u128, base: u32) -> (Vec<u128>,HashMap<u32,u32>) {
-
+fn process_range_detailed(n_start: u128, n_end: u128, base: u32) -> (Vec<u128>, HashMap<u32, u32>) {
     // near_misses_cutoff: minimum number of uniques required for the number to be recorded
     let near_misses_cutoff: u32 = (base as f32 * 0.9) as u32;
 
     // near_misses: list of numbers with niceness ratio (uniques/base) above the cutoff
     // pre-allocate memory for the maximum possible number of near misses (wastes memory but saves resizing)
     let mut near_misses: Vec<u128> = Vec::with_capacity((n_end - n_start) as usize);
-    
+
     // qty_uniques: the quantity of numbers with each possible niceness
     let mut qty_uniques: Vec<u32> = vec![0; base as usize];
 
     // loop for all items in range (try to optimize anything in here)
-    for num in n_start..n_end { 
-
+    for num in n_start..n_end {
         // get the number of uniques in the sqube
         let num_uniques: u32 = get_num_uniques(Natural::from(num), base);
 
@@ -222,12 +216,12 @@ fn process_range_detailed(n_start: u128, n_end: u128, base: u32) -> (Vec<u128>,H
 
     // build the initial values (api expects it)
     let mut dict_qty_uniques: HashMap<u32, u32> = HashMap::new();
-    for (num, count) in qty_uniques.iter().enumerate() { 
+    for (num, count) in qty_uniques.iter().enumerate() {
         dict_qty_uniques.insert(num as u32 + 1, *count);
     }
 
     // return it as a tuple
-    return (near_misses, dict_qty_uniques)
+    return (near_misses, dict_qty_uniques);
 }
 
 #[test]
@@ -235,9 +229,7 @@ fn test_process_range_detailed() {
     assert_eq!(
         process_range_detailed(47, 100, 10),
         (
-            Vec::from([
-                69,
-            ]),
+            Vec::from([69,]),
             HashMap::from([
                 (1, 0),
                 (2, 0),
@@ -275,55 +267,57 @@ fn test_process_range_detailed() {
 }
 
 fn main() {
-
     // parse args from command line
     let cli = Cli::parse();
 
     // get the field to search
-    let claim_data = if cli.benchmark { get_field_benchmark() } else { get_field_detailed(&cli.api_base, &cli.username) };
+    let claim_data = if cli.benchmark {
+        get_field_benchmark()
+    } else {
+        get_field_detailed(&cli.api_url, &cli.username, &cli.base, &cli.max_range)
+    };
 
     // print debug information
-    if ! cli.quiet { println!("{:?}", claim_data); }
+    if !cli.quiet {
+        println!("{:?}", claim_data);
+    }
 
     // start a timer
     let before = Instant::now();
 
     // search for near_misses and qty_uniques
-    let (
-        near_misses, 
-        qty_uniques
-    ) = process_range_detailed(
+    let (near_misses, qty_uniques) = process_range_detailed(
         claim_data.search_start,
         claim_data.search_end,
         claim_data.base,
     );
 
     // debug: print the timer
-    if cli.benchmark { println!("Elapsed time: {:.4?}", before.elapsed()); }
+    if cli.benchmark {
+        println!("Elapsed time: {:.4?}", before.elapsed());
+    }
 
     // convert the near_misses list into a map of {num, uniques}
-    let mut near_miss_map: HashMap<u128,u32> = HashMap::new();
+    let mut near_miss_map: HashMap<u128, u32> = HashMap::new();
     for nm in near_misses.iter() {
-        near_miss_map.insert(
-            *nm,
-            get_num_uniques(
-                Natural::from(*nm),
-                claim_data.base
-            )
-        );
+        near_miss_map.insert(*nm, get_num_uniques(Natural::from(*nm), claim_data.base));
     }
 
     // compile results
-    let submit_data = FieldSubmit { 
+    let submit_data = FieldSubmit {
         id: claim_data.id,
         username: &cli.username,
         client_version: &CLIENT_VERSION,
         unique_count: qty_uniques,
-        near_misses: near_miss_map
+        near_misses: near_miss_map,
     };
     // print debug information
-    if ! cli.quiet { println!("{:?}", submit_data); }
-    
+    if !cli.quiet {
+        println!("{:?}", submit_data);
+    }
+
     // upload results (only if not doing benchmarking)
-    if ! cli.benchmark { submit_field_detailed(&cli.api_base, submit_data) }
+    if !cli.benchmark {
+        submit_field_detailed(&cli.api_url, submit_data)
+    }
 }
