15010988921c681be3310cbabf328c7b8d2a0518
wasabipesto <21313833+wasabipesto@users.noreply.github.com>
2023-04-13 11:31:50 -0400

    new integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,36 @@
 extern crate nice_rust;
 
 #[test]
+fn integration_niceonly_benchmark() {
+    nice_rust::run(
+        nice_rust::Mode::Niceonly,
+        "https://nicenumbers.net/api".to_string(),
+        "anonymous".to_string(),
+        false,
+        false,
+        true,
+        None,
+        Some(1000000),
+        None,
+    );
+}
+
+#[test]
+fn integration_detailed_benchmark() {
+    nice_rust::run(
+        nice_rust::Mode::Detailed,
+        "https://nicenumbers.net/api".to_string(),
+        "anonymous".to_string(),
+        false,
+        false,
+        true,
+        None,
+        Some(100000),
+        None,
+    );
+}
+
+#[test]
 fn integration_niceonly() {
     nice_rust::run(
         nice_rust::Mode::Niceonly,
